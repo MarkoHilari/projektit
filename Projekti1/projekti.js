c1387216718630eae7e3625d0975a5228ba99e31
MikaHuo <mika.huotari@edu.keuda.fi>
2022-10-04 14:21:39 +0300

    js

--- Projekti1/projekti.js
+++ Projekti1/projekti.js
@@ -1,1 +1,76 @@
-function Tarkasta
+function tarkasta(form)
+{
+var etunimi = form.enimi.value;
+var sukunimi = form.snimi.value;
+var sahkoposti = form.email.value;
+var palaute = form.palaute.value;
+var mainos = form.posti;
+var koulu = form.koulu;
+
+if(etunimi.lenght < 3)
+{
+  alert("Anna pidempi nimi");
+  form.enimi.focus();
+  return false;
+}
+if(sukunimi.lenght < 3)
+{
+  alert("Anna pidempi sukunimi");
+  form.snimi.focus();
+  return false;
+}
+  var ehdot = /\S+@\S+/;
+  if(!ehdot.test(sahkoposti))
+{
+  alert("Anna kunnollinen sähköposti");
+  form.email.focus();
+  return false;
+}
+  if(palaute.lenght < 15)
+{
+  alert("Palautteen pitää olla vähintään 15 merkkiä!")
+  form.palaute.focus();
+  return false;
+}
+  var vastaus = false;
+  for(var i = 0; i < posti.lenght; i++)
+{
+  if(posti[i].checked == true)
+{
+  vastaus = true;
+}
+}
+  if(vastaus == false)
+{
+  alert('Valitse haluatko mainoksia');
+  return false;
+}
+  var tarkastavastaus = false;
+  for(var j = 0; j < check.lenght; j++)
+{
+  if(check[j].checked == true)
+
+{
+   tarkastavastaus = true;
+ }
+}
+if(tarkastavastaus == false)
+{
+ alert("Et valinnut, mikÃ¤ on kivaa");
+ return false;
+}
+else
+{
+ alert("Kiitos lomakkeen tÃ¤ytÃ¶stÃ¤");
+
+}
+}
+
+function tyhjenna(lomake)
+{
+var clean = lomake.value;
+if(!confirm("Oletko varma?"))
+{
+ return false;
+}
+}
