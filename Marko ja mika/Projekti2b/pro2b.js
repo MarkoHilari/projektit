0835d0aa768e96e14b15fbec034feda93c2e9a0a
MarkoHilari <marko.hilari@edu.keuda.fi>
2022-10-14 10:02:26 +0300

    Pro2B

---
+++ Marko ja mika/Projekti2b/pro2b.js
@@ -0,0 +1,13 @@
+function lahetaVastaus()
+var yhteensa = 5;
+var pisteet = 0;
+var huom = 0;
+var k1 = document.forms["lomake"]["k1"].value;
+var k2 = document.forms["lomake"]["k2"].value;
+var k3 = document.forms["lomake"]["k3"].value;
+var k4 = document.forms["lomake"]["k4"].value;
+var k5 = document.forms["lomake"]["k5"].value;
+
+{
+
+}
