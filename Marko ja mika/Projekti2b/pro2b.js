fbf9fb696302df6f1b88176f8aa48354c5cf424c
MarkoHilari <marko.hilari@edu.keuda.fi>
2022-10-14 12:17:00 +0300

    Korjaukset

--- Marko ja mika/Projekti2b/pro2b.js
+++ Marko ja mika/Projekti2b/pro2b.js
@@ -1,11 +1,11 @@
 var yhteensa = 5;
 var pisteet = 0;
 var huom = 0;
-var k1 = document.forms["lomake"]["k1"].value;
-var k2 = document.forms["lomake"]["k2"].value;
-var k3 = document.forms["lomake"]["k3"].value;
-var k4 = document.forms["lomake"]["k4"].value;
-var k5 = document.forms["lomake"]["k5"].value;
+var k1 = document.forms["kyselyLomake"]["k1"].value;
+var k2 = document.forms["kyselyLomake"]["k2"].value;
+var k3 = document.forms["kyselyLomake"]["k3"].value;
+var k4 = document.forms["kyselyLomake"]["k4"].value;
+var k5 = document.forms["kyselyLomake"]["k5"].value;
 for(i = 1; i <= yhteensa; i++)
 {
    if(eval('k'+i) == null || eval ('k'+i) == '')
@@ -25,6 +25,6 @@ for(i = 1; i <= yhteensa; i++)
    }
    var tulokset = document.getElementById('tulokset');
    alert('sait ' + pisteet+ ' pistettä, kun maksimi pistemäärä oli ' + yhteensa);
-   tulokset.innerHTML = '<h3>Sait <span>' + pisteet + </span> pistettä, kun maksimi pistemäärä oli <span> + yhteensa '</span></h3>';
+   tulokset.innerHTML = '<h3>Sait <span>' + pisteet + '</span> pistettä, kun maksimi pistemäärä oli <span>' + yhteensa '</span></h3>';
    return false;
 }
