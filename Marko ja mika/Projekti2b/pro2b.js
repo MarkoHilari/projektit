320c5c91c1cddb23f29233723c1a226576ead2de
MarkoHilari <marko.hilari@edu.keuda.fi>
2022-10-14 10:44:50 +0300

    Pro2b

--- Marko ja mika/Projekti2b/pro2b.js
+++ Marko ja mika/Projekti2b/pro2b.js
@@ -7,7 +7,24 @@ var k2 = document.forms["lomake"]["k2"].value;
 var k3 = document.forms["lomake"]["k3"].value;
 var k4 = document.forms["lomake"]["k4"].value;
 var k5 = document.forms["lomake"]["k5"].value;
-
+for(i = 1; i <= yhteensa; i++)
 {
-
+   if(eval('k'+i) == null || eval ('k'+i) == '')
+   {
+     alert('Et vastannut kysymykseen numero' + i);
+     huom = 1;
+   }
+   if(huom == 1) return false;
+   var vastaukset = ["b", "a", "d", "b", "d"];
+   for(i = 1; i <= yhteensa; i++)
+   {
+       if(eval('k'+i) == vastaukset[i-1])
+       {
+         pisteet++;
+       }
+   }
+   var tulokset = document.getElementById('tulokset');
+   alert('sait ' + pisteet+ ' pistettä, kun maksimi pistemäärä oli ' + yhteensa);
+   tulokset.innerHTML = '<h3>Sait <span>' + pisteet + </span> pistettä, kun maksimi pistemäärä oli <span> + yhteensa '</span></h3>';
+   return false;
 }
