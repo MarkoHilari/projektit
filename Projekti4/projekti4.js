c5f94d453662d71686a24a5509902fdc652f4b68
Mika Huotari <mika.huotari@edu.keuda.fi>
2022-10-31 11:10:45 +0200

    melkein redi

--- Projekti4/projekti4.js
+++ Projekti4/projekti4.js
@@ -1,1 +1,47 @@
-function
+let painallus = document.getElementById('painike');
+painallus.addEventListener("click", lisaaLista);
+
+let syotto = document.getElementById('syotto');
+syotto.addEventListener("keypress", lisaaListaanE);
+
+let ul = document.querySelector("ul");
+
+function inputLength()
+{
+  return syotto.value.length;
+}
+function luoLista()
+{
+   let li = document.createElement("li");
+   if(syotto.value != '')
+   {
+     li.appendChild(document.createTextNode(syotto.value));
+     ul.appendChild(li);
+     syotto.value = "";
+   }
+   function ulostulo()
+   {
+     li.classList.toggle("valmis");
+   }
+   li.addEventListener("click", valmis);
+   let poisto = document.createElement("button");
+   poisto.appendChild(document.TextNode("xxx"));
+   li.appendChild(poisto);
+   poisto.addEventListener("click", vahennaTaakkaa);
+   function vahennaTaakkaa()
+   {
+     li.classList.add("delete") //muuta tätä jos mahdollista
+   }
+ }
+   function lisaaLista()
+   {
+     if (inputLength() > 0){luoLista();
+     }
+   }
+   function lisaaListaanE(event)
+   {
+     if (inputLength() > 0 && event.which === 13)
+     {
+       lisaaListaanE();
+     }
+   }
