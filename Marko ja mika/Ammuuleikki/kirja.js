95f0a3380ba9a94c8f854a369927dfbb21f002cb
MarkoHilari <marko.hilari@edu.keuda.fi>
2022-11-03 16:22:19 +0200

    Ammuleikki toimii

--- Marko ja mika/Ammuuleikki/kirja.js
+++ Marko ja mika/Ammuuleikki/kirja.js
@@ -1,8 +1,17 @@
-var audio = new Audio("kissa.mp3");
-function kissa()
+/*function aanet(elain)
+var audio = new audio();
 {
     audio.play();
 }
   onmouseout= function(){
 		audio.pause();
+}*/
+function aanet(elain)
+{
+  aani = elain.id;
+  var audio = new Audio(aani+'.mp3');
+  audio.play();
+  onmouseout= function(){
+		audio.pause();
+}
 }
