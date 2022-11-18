cbf0e951276103c52c2fa83115786ec56d061e81
MarkoHilari <marko.hilari@edu.keuda.fi>
2022-11-18 12:50:56 +0200

    Komments

--- Projekti4/projekti4.js
+++ Projekti4/projekti4.js
@@ -1,69 +1,67 @@
-/* Haetaan lomakkeelta painike ja laitetaan sille clickauksen 'kuuntelu' */
+
+/* Luodaan enter toiminto, funktiolla kutusutaan toimintoa*/
   let enterButton = document.getElementById("painike");
-  enterButton.addEventListener("click",addListClick); // PerÃ¤sssÃ¤ funktio, jota kutsutaan
+  enterButton.addEventListener("click",addListClick);
 
-/* Haetaan lomakkeelta syÃ¶ttÃ¶kenttÃ¤ ja laitetaan sille enterin 'kuuntelu' */
+/* Haetaan syöttökentästä tiedot, funktiolla kutsutaan */
   let input = document.getElementById("syotto");
-  input.addEventListener("keypress", addEnter); // PerÃ¤ssÃ¤ funktio, jota kutsutaan
-
-/* Haetaan lomakkeelta jÃ¤rjestÃ¤mÃ¤tÃ¶n lista, johon tehtÃ¤vÃ¤t syÃ¶tetÃ¤Ã¤n */
+  input.addEventListener("keypress", addEnter);
+/* Luodaan järjestämättömän listan alku  */
   let ul = document.querySelector("ul");
 
-/* Luodaan funktio, joka palauttaa syÃ¶tetyn merkkijonon pituuden */
+/* Luodaan tarkatus, että syöttökentässä on jotain tietoa */
 function inputLength()
 {
 	return input.value.length;
 }
 
-/* Varsinainen lisÃ¤ysfunktio */
+/* Tehdään funktio joka lisää li elementin */
 function addList()
 {
-	let li = document.createElement("li"); // luo "li" elementin
-  // Tarkistetaan, ettÃ¤ on syÃ¶tetty jotain
+	let li = document.createElement("li");
+  // Tarkistus, että on syötetty jotain tietoa
   if(input.value != '')
   {
-    li.appendChild(document.createTextNode(input.value)); //lisÃ¤tÃ¤Ã¤n li-elementtiin tekstin, joka on laatikossa
-  	ul.appendChild(li); //lisÃ¤Ã¤ li-elementin ul-elementtiin
-  	input.value = ""; // TyhjentÃ¤Ã¤ syÃ¶ttÃ¶kentÃ¤n
+    li.appendChild(document.createTextNode(input.value)); //lisää tiedot jotka on syötetty
+  	ul.appendChild(li); //lisää li:n ul
+  	input.value = ""; // resetoi syöttökentän
   }
 
-	// Aloitetaan tehdyn tehtÃ¤vÃ¤n vÃ¤rjÃ¤ys
-	// koska se on funktiossa, se tekee sen vain lisÃ¤ttyihin tehtÃ¤viin
+	/* muutetaan tehtävät tehdyksi*/
 	function doneTask() {
 		li.classList.toggle("done");
 	}
-  // Kutsutaan yllÃ¤olevaa funktiota
+  // funktion kutsu
 	li.addEventListener("click",doneTask);
-	//Tehdyn tehtÃ¤vÃ¤n vÃ¤rjÃ¤ys loppuu
+	//Funktion loppu
 
 
-	// Aloitetaan poistopainikkeen lisÃ¤ys
-	let poisto = document.createElement("button"); // luodaan painike-elementti
-	poisto.appendChild(document.createTextNode("X")); // LisÃ¤tÃ¤Ã¤n siihen X-kirjain
-	li.appendChild(poisto); // LisÃ¤tÃ¤Ã¤n se kaikkiin li-elementteihin
+	// Poistopainike "X"
+	let poisto = document.createElement("button"); // luodaan painike-muuttuja
+	poisto.appendChild(document.createTextNode("X")); // Poistoelementtin X kirjain
+	li.appendChild(poisto); // X nappi poistettaviin tehtäviin li
 	poisto.addEventListener("click", removeMissionImpossible);
-	// Poistopainikkeen lisÃ¤ys loppuu
 
 
-	//LisÃ¤tÃ¤Ã¤n varsinainen poistofunktio (DISPLAY: NONE)
+	//Poistifunktio
 	function removeMissionImpossible()
   {
 		li.classList.add("delete")
 	}
-}// PÃ¤Ã¤funktio pÃ¤Ã¤ttyy
+}
 
-
+// Lisätään listaan klikkauksella funktio
 function addListClick()
 {
-	if (inputLength() > 0) { //tarkistaa, ettÃ¤ on syÃ¶tetty jotain
-		addList(); // Kutsuu 'pÃ¤Ã¤funktiota'
+	if (inputLength() > 0) { //Tarkistus että on syötetty jotain tietoa
+		addList(); // Kutsuu addList funktio, joka on yllä mainittu
 	}
 }
 
 function addEnter(event)
 {
-	if (inputLength() > 0 && event.which ===13) { //Tarkistetaan, ettÃ¤ Enter on painettu
-		//nro 13 on Enterin avainkoodi, voidaan kirjoittaa myÃ¶s event.keyCode === 13
-		addList(); // Kutsuu 'pÃ¤Ã¤funktiota'
+	if (inputLength() > 0 && event.which === 13) { //Enterin painallus
+		//nro 13 on Enterin avainkoodi
+		addList(); // Kutsuu addList funktio, joka on yllä mainittu
 	}
 }
