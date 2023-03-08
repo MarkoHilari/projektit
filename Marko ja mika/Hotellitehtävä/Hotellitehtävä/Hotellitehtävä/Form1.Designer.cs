ad52acc94941891578d75f7a9921256a8af0db84
MarkoHilari <marko.hilari@edu.keuda.fi>
2023-03-08 14:10:44 +0200

    Alku

--- Marko ja mika/Hotellitehtävä/Hotellitehtävä/Hotellitehtävä/Form1.Designer.cs
+++ Marko ja mika/Hotellitehtävä/Hotellitehtävä/Hotellitehtävä/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Hotellitehtävä
 {
-    partial class Form1
+    partial class kirjausFM
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(kirjausFM));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tunnistauduGB = new System.Windows.Forms.GroupBox();
+            this.ktunnusLB = new System.Windows.Forms.Label();
+            this.salasanaLB = new System.Windows.Forms.Label();
+            this.ktunnusTB = new System.Windows.Forms.TextBox();
+            this.ssTB = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tunnistauduGB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(784, 338);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(1, 356);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(806, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MistyRose;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(0, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(795, 57);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Kirjaudu sisään hotellijärjestelmään!";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tunnistauduGB
+            // 
+            this.tunnistauduGB.Controls.Add(this.ssTB);
+            this.tunnistauduGB.Controls.Add(this.ktunnusTB);
+            this.tunnistauduGB.Controls.Add(this.salasanaLB);
+            this.tunnistauduGB.Controls.Add(this.ktunnusLB);
+            this.tunnistauduGB.Location = new System.Drawing.Point(108, 479);
+            this.tunnistauduGB.Name = "tunnistauduGB";
+            this.tunnistauduGB.Size = new System.Drawing.Size(576, 142);
+            this.tunnistauduGB.TabIndex = 2;
+            this.tunnistauduGB.TabStop = false;
+            // 
+            // ktunnusLB
+            // 
+            this.ktunnusLB.AutoSize = true;
+            this.ktunnusLB.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ktunnusLB.Location = new System.Drawing.Point(29, 40);
+            this.ktunnusLB.Name = "ktunnusLB";
+            this.ktunnusLB.Size = new System.Drawing.Size(179, 26);
+            this.ktunnusLB.TabIndex = 0;
+            this.ktunnusLB.Text = "Käyttäjätunnus:";
+            // 
+            // salasanaLB
+            // 
+            this.salasanaLB.AutoSize = true;
+            this.salasanaLB.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salasanaLB.Location = new System.Drawing.Point(29, 87);
+            this.salasanaLB.Name = "salasanaLB";
+            this.salasanaLB.Size = new System.Drawing.Size(104, 26);
+            this.salasanaLB.TabIndex = 1;
+            this.salasanaLB.Text = "Salasana:";
+            // 
+            // ktunnusTB
+            // 
+            this.ktunnusTB.Location = new System.Drawing.Point(270, 40);
+            this.ktunnusTB.Name = "ktunnusTB";
+            this.ktunnusTB.Size = new System.Drawing.Size(185, 26);
+            this.ktunnusTB.TabIndex = 2;
+            this.ktunnusTB.Text = "Admin";
+            // 
+            // ssTB
+            // 
+            this.ssTB.Location = new System.Drawing.Point(270, 87);
+            this.ssTB.Name = "ssTB";
+            this.ssTB.Size = new System.Drawing.Size(185, 26);
+            this.ssTB.TabIndex = 3;
+            this.ssTB.Text = "Password";
+            this.ssTB.UseSystemPasswordChar = true;
+            // 
+            // kirjausFM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(808, 676);
+            this.Controls.Add(this.tunnistauduGB);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "kirjausFM";
+            this.Text = "Kirjautumisikkuna";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tunnistauduGB.ResumeLayout(false);
+            this.tunnistauduGB.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox tunnistauduGB;
+        private System.Windows.Forms.TextBox ssTB;
+        private System.Windows.Forms.TextBox ktunnusTB;
+        private System.Windows.Forms.Label salasanaLB;
+        private System.Windows.Forms.Label ktunnusLB;
     }
 }
 
--- Marko ja mika/Hotellitehtävä/Hotellitehtävä/Hotellitehtävä/Form1.cs
+++ Marko ja mika/Hotellitehtävä/Hotellitehtävä/Hotellitehtävä/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Hotellitehtävä
 {
-    public partial class Form1 : Form
+    public partial class kirjausFM : Form
     {
-        public Form1()
+        public kirjausFM()
         {
             InitializeComponent();
         }
