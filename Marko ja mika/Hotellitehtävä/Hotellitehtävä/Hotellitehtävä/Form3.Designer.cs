9cf658a1bbd36eeb668516d750baa20df8b68e9a
mika huotari <mika.huotari@edu.keuda.fi>
2023-03-09 10:46:26 +0200

    muutoksia

--- Marko ja mika/Hotellitehtävä/Hotellitehtävä/Hotellitehtävä/Form3.Designer.cs
+++ Marko ja mika/Hotellitehtävä/Hotellitehtävä/Hotellitehtävä/Form3.Designer.cs
@@ -71,130 +71,138 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(557, 154);
+            this.dataGridView1.Location = new System.Drawing.Point(594, 172);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 82;
             this.dataGridView1.RowTemplate.Height = 33;
-            this.dataGridView1.Size = new System.Drawing.Size(678, 512);
+            this.dataGridView1.Size = new System.Drawing.Size(767, 612);
             this.dataGridView1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("MV Boli", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(37, 176);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 31);
+            this.label1.Size = new System.Drawing.Size(161, 34);
             this.label1.TabIndex = 2;
             this.label1.Text = "Varaus nro:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("MV Boli", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(37, 222);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(226, 31);
+            this.label2.Size = new System.Drawing.Size(238, 34);
             this.label2.TabIndex = 3;
             this.label2.Text = "Henkilökunta nro:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("MV Boli", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(37, 278);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(174, 31);
+            this.label3.Size = new System.Drawing.Size(181, 34);
             this.label3.TabIndex = 4;
             this.label3.Text = "Huonetyyppi:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("MV Boli", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(37, 330);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(178, 31);
+            this.label4.Size = new System.Drawing.Size(184, 34);
             this.label4.TabIndex = 5;
             this.label4.Text = "Huoneen nro:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Font = new System.Drawing.Font("MV Boli", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(37, 369);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(105, 31);
+            this.label5.Size = new System.Drawing.Size(101, 34);
             this.label5.TabIndex = 6;
             this.label5.Text = "Sisään:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Font = new System.Drawing.Font("MV Boli", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(37, 414);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(77, 31);
+            this.label6.Size = new System.Drawing.Size(76, 34);
             this.label6.TabIndex = 7;
             this.label6.Text = "Ulos:";
             // 
             // textBox2
             // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.Location = new System.Drawing.Point(281, 172);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(260, 31);
+            this.textBox2.Size = new System.Drawing.Size(260, 38);
             this.textBox2.TabIndex = 8;
             // 
             // comboBox1
             // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(281, 220);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(260, 33);
+            this.comboBox1.Size = new System.Drawing.Size(260, 39);
             this.comboBox1.TabIndex = 9;
             // 
             // comboBox2
             // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox2.FormattingEnabled = true;
             this.comboBox2.Location = new System.Drawing.Point(281, 271);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(260, 33);
+            this.comboBox2.Size = new System.Drawing.Size(260, 39);
             this.comboBox2.TabIndex = 10;
             // 
             // comboBox3
             // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox3.FormattingEnabled = true;
             this.comboBox3.Location = new System.Drawing.Point(281, 323);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(260, 33);
+            this.comboBox3.Size = new System.Drawing.Size(260, 39);
             this.comboBox3.TabIndex = 11;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(281, 369);
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(281, 384);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(260, 31);
+            this.dateTimePicker1.Size = new System.Drawing.Size(260, 38);
             this.dateTimePicker1.TabIndex = 12;
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(281, 415);
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(281, 446);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(260, 31);
+            this.dateTimePicker2.Size = new System.Drawing.Size(260, 38);
             this.dateTimePicker2.TabIndex = 13;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 506);
+            this.button1.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(57, 581);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(212, 50);
+            this.button1.Size = new System.Drawing.Size(175, 50);
             this.button1.TabIndex = 14;
             this.button1.Text = "Lisää uusi varaus";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(241, 506);
+            this.button2.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(265, 581);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(114, 50);
             this.button2.TabIndex = 15;
@@ -203,20 +211,22 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(410, 506);
+            this.button3.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(427, 581);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(114, 50);
             this.button3.TabIndex = 16;
-            this.button3.Text = "button3";
+            this.button3.Text = "Poista";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(213, 585);
+            this.button4.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(395, 659);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(114, 50);
+            this.button4.Size = new System.Drawing.Size(146, 50);
             this.button4.TabIndex = 17;
-            this.button4.Text = "button4";
+            this.button4.Text = "Tyhjennä kentät";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // Form4
@@ -224,7 +234,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Hotellitehtävä.Properties.Resources.hotelli1;
-            this.ClientSize = new System.Drawing.Size(1247, 678);
+            this.ClientSize = new System.Drawing.Size(1401, 827);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
