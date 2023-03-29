f82587e52b14128e3ec863658fcb42d4d4687a6e
MarkoHilari <marko.hilari@edu.keuda.fi>
2023-03-29 13:14:54 +0300

    koko

--- Marko ja mika/Pizzatehtava/Pizzeria/Pizzeria/Aloitussivu.cs
+++ Marko ja mika/Pizzatehtava/Pizzeria/Pizzeria/Aloitussivu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Pizzeria
 {
-    public partial class Form1 : Form
+    public partial class Aloitussivu : Form
     {
-        public Form1()
+        public Aloitussivu()
         {
             InitializeComponent();
         }
