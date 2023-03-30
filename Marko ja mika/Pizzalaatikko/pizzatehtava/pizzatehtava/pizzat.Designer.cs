0064ec5462e68e5d95f806fe5e00faaa60d2848b
MarkoHilari <marko.hilari@edu.keuda.fi>
2023-03-30 09:27:07 +0300

    joo

--- Marko ja mika/Pizzalaatikko/pizzatehtava/pizzatehtava/pizzat.Designer.cs
+++ Marko ja mika/Pizzalaatikko/pizzatehtava/pizzatehtava/pizzat.Designer.cs
@@ -430,7 +430,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(312, 39);
+            this.label1.Location = new System.Drawing.Point(312, 55);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(600, 68);
             this.label1.TabIndex = 2;
