2572f0d64e43d52bfb2f6c47145a3fe2bdf1b259
mika huotari <mika.huotari@edu.keuda.fi>
2023-03-31 09:22:43 +0300

    ostoskori

--- Marko ja mika/Pizzalaatikko/pizzatehtava/pizzatehtava/YHDISTA.cs
+++ Marko ja mika/Pizzalaatikko/pizzatehtava/pizzatehtava/YHDISTA.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace pizzatehtava
 {
     internal class YHDISTA
     {
+        private MySqlConnection yhdista = new MySqlConnection("datasource= localhost; port=3306; username=root; password=; database=pizzatieto");
+    
+        public MySqlConnection otaYhteys()
+        { 
+            return yhdista;
+        }
+        public void avaaYhteys()
+        {
+            if(yhdista.State == ConnectionState.Closed)
+            {
+                yhdista.Open();
+            }
+        }
+        public void avaaXhteys()
+        {
+            if(yhdista.State == ConnectionState.Closed)
+            {
+                yhdista.Open();
+            }
+        }
+        public void suljeYhteys()
+        {
+            if(yhdista.State == ConnectionState.Open)
+            {
+                yhdista.Close();
+            }
+        }
+
     }
 }
