c681d46eb9a92b05b8d41a1be9d586800b2e175f
MarkoHilari <marko.hilari@edu.keuda.fi>
2023-04-06 11:05:12 +0300

    Pizza

--- Marko ja mika/Pizzalaatikko/pizzatehtava/pizzatehtava/Paaikkuna.Designer.cs
+++ Marko ja mika/Pizzalaatikko/pizzatehtava/pizzatehtava/Paaikkuna.Designer.cs
@@ -58,20 +58,18 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(9, 10);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1524, 210);
+            this.panel1.Size = new System.Drawing.Size(1143, 168);
             this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Handwriting", 28.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(262, 63);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(196, 50);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(953, 97);
+            this.label1.Size = new System.Drawing.Size(721, 74);
             this.label1.TabIndex = 2;
             this.label1.Text = "Mehmet 3. Pizza sulle";
             // 
@@ -79,10 +77,9 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(1282, 15);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(962, 12);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(224, 185);
+            this.pictureBox2.Size = new System.Drawing.Size(168, 148);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
@@ -90,10 +87,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(20, 15);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(15, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(184, 185);
+            this.pictureBox1.Size = new System.Drawing.Size(138, 148);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -101,10 +97,10 @@
             // pizzaBT
             // 
             this.pizzaBT.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pizzaBT.Location = new System.Drawing.Point(60, 21);
-            this.pizzaBT.Margin = new System.Windows.Forms.Padding(6);
+            this.pizzaBT.Location = new System.Drawing.Point(45, 17);
+            this.pizzaBT.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pizzaBT.Name = "pizzaBT";
-            this.pizzaBT.Size = new System.Drawing.Size(242, 90);
+            this.pizzaBT.Size = new System.Drawing.Size(182, 72);
             this.pizzaBT.TabIndex = 1;
             this.pizzaBT.Text = "Pizza Meny";
             this.pizzaBT.UseVisualStyleBackColor = true;
@@ -113,10 +109,10 @@
             // juomatBT
             // 
             this.juomatBT.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.juomatBT.Location = new System.Drawing.Point(430, 21);
-            this.juomatBT.Margin = new System.Windows.Forms.Padding(6);
+            this.juomatBT.Location = new System.Drawing.Point(322, 17);
+            this.juomatBT.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.juomatBT.Name = "juomatBT";
-            this.juomatBT.Size = new System.Drawing.Size(242, 90);
+            this.juomatBT.Size = new System.Drawing.Size(182, 72);
             this.juomatBT.TabIndex = 2;
             this.juomatBT.Text = "Juomat";
             this.juomatBT.UseVisualStyleBackColor = true;
@@ -124,10 +120,10 @@
             // ostosBT
             // 
             this.ostosBT.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ostosBT.Location = new System.Drawing.Point(812, 21);
-            this.ostosBT.Margin = new System.Windows.Forms.Padding(6);
+            this.ostosBT.Location = new System.Drawing.Point(609, 17);
+            this.ostosBT.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ostosBT.Name = "ostosBT";
-            this.ostosBT.Size = new System.Drawing.Size(242, 90);
+            this.ostosBT.Size = new System.Drawing.Size(182, 72);
             this.ostosBT.TabIndex = 3;
             this.ostosBT.Text = "Ostoskori";
             this.ostosBT.UseVisualStyleBackColor = true;
@@ -140,21 +136,21 @@
             this.groupBox1.Controls.Add(this.juomatBT);
             this.groupBox1.Controls.Add(this.ostosBT);
             this.groupBox1.Controls.Add(this.pizzaBT);
-            this.groupBox1.Location = new System.Drawing.Point(32, 671);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.groupBox1.Location = new System.Drawing.Point(24, 537);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(6);
-            this.groupBox1.Size = new System.Drawing.Size(1486, 123);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(1114, 98);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             // 
             // suljeBT
             // 
             this.suljeBT.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.suljeBT.Location = new System.Drawing.Point(1200, 21);
-            this.suljeBT.Margin = new System.Windows.Forms.Padding(6);
+            this.suljeBT.Location = new System.Drawing.Point(900, 17);
+            this.suljeBT.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.suljeBT.Name = "suljeBT";
-            this.suljeBT.Size = new System.Drawing.Size(242, 90);
+            this.suljeBT.Size = new System.Drawing.Size(182, 72);
             this.suljeBT.TabIndex = 4;
             this.suljeBT.Text = "Sulje";
             this.suljeBT.UseVisualStyleBackColor = true;
@@ -164,10 +160,10 @@
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.Image = global::pizzatehtava.Properties.Resources.pizza_car;
-            this.pictureBox3.Location = new System.Drawing.Point(1162, 252);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox3.Location = new System.Drawing.Point(872, 202);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(356, 237);
+            this.pictureBox3.Size = new System.Drawing.Size(267, 190);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 5;
             this.pictureBox3.TabStop = false;
@@ -178,10 +174,10 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.Window;
-            this.label2.Location = new System.Drawing.Point(476, 408);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(357, 326);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 40);
+            this.label2.Size = new System.Drawing.Size(99, 29);
             this.label2.TabIndex = 6;
             this.label2.Text = "Osoite:";
             // 
@@ -191,10 +187,10 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.Window;
-            this.label3.Location = new System.Drawing.Point(446, 483);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(334, 386);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(160, 40);
+            this.label3.Size = new System.Drawing.Size(123, 29);
             this.label3.TabIndex = 7;
             this.label3.Text = "Puhelin:";
             // 
@@ -204,10 +200,10 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.Window;
-            this.label4.Location = new System.Drawing.Point(380, 558);
-            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Location = new System.Drawing.Point(285, 446);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(225, 40);
+            this.label4.Size = new System.Drawing.Size(173, 29);
             this.label4.TabIndex = 8;
             this.label4.Text = "Savumerkki:";
             // 
@@ -217,10 +213,10 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.Window;
-            this.label5.Location = new System.Drawing.Point(686, 408);
-            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Location = new System.Drawing.Point(514, 326);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(460, 40);
+            this.label5.Size = new System.Drawing.Size(355, 29);
             this.label5.TabIndex = 9;
             this.label5.Text = "Karvakuja 69 Itä-Helsinki";
             // 
@@ -230,10 +226,10 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.Window;
-            this.label6.Location = new System.Drawing.Point(686, 483);
-            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Location = new System.Drawing.Point(514, 386);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(260, 40);
+            this.label6.Size = new System.Drawing.Size(200, 29);
             this.label6.TabIndex = 10;
             this.label6.Text = "555-pizzatime";
             // 
@@ -243,20 +239,20 @@
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Lucida Handwriting", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.Window;
-            this.label7.Location = new System.Drawing.Point(686, 558);
-            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Location = new System.Drawing.Point(514, 446);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(398, 40);
+            this.label7.Size = new System.Drawing.Size(307, 29);
             this.label7.TabIndex = 11;
             this.label7.Text = "Uksi lyhyt toinen pitka";
             // 
             // Paaikkuna
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::pizzatehtava.Properties.Resources.food_and_drink_pizza_pizzas_tomato;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1554, 808);
+            this.ClientSize = new System.Drawing.Size(1166, 646);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -267,8 +263,8 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Paaikkuna";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "d";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
