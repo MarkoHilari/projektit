f7a00fd440e159b2e592faf02b3b3f246629480e
MarkoHilari <marko.hilari@edu.keuda.fi>
2023-04-20 10:12:53 +0300

    Vari

--- Marko ja mika/Autovuokra/AutoVuokra/AutoVuokra/Lataus.Designer.cs
+++ Marko ja mika/Autovuokra/AutoVuokra/AutoVuokra/Lataus.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Lataus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Lataus";
             this.Text = "Lataus";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
