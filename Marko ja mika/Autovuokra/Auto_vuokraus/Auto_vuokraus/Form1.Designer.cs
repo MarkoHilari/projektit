a56662858ba80178249878b6836e99564eb0a530
MikaHuo <111268804+MikaHuo@users.noreply.github.com>
2023-04-21 09:08:25 +0300

    kuva kansio ja 1 kuva

--- Marko ja mika/Autovuokra/Auto_vuokraus/Auto_vuokraus/Form1.Designer.cs
+++ Marko ja mika/Autovuokra/Auto_vuokraus/Auto_vuokraus/Form1.Designer.cs
@@ -35,30 +35,30 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(298, 159);
+            this.label1.Location = new System.Drawing.Point(265, 127);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.Size = new System.Drawing.Size(48, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "HOMO";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(492, 281);
+            this.button1.Location = new System.Drawing.Point(458, 273);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 67);
             this.button1.TabIndex = 1;
             this.button1.Text = "Mulkku";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
