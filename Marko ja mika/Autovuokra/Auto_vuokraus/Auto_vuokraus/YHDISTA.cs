d176eeda141499b61bcf3b535f9a53f64450d7db
MikaHuo <111268804+MikaHuo@users.noreply.github.com>
2023-04-24 13:56:12 +0300

    savetus

--- Marko ja mika/Autovuokra/Auto_vuokraus/Auto_vuokraus/YHDISTA.cs
+++ Marko ja mika/Autovuokra/Auto_vuokraus/Auto_vuokraus/YHDISTA.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+using MySql.data.MySqlClient;
 namespace Auto_vuokraus
 {
 
