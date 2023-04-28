4b1358bb35c819e369a0a8f19637d9cdee6cde0f
MikaHuo <111268804+MikaHuo@users.noreply.github.com>
2023-04-28 10:54:13 +0300

    Revert "ooo"
    This reverts commit f4052c2f63b7001a962e3a6ff7bb075697276901.

--- Marko ja mika/Autovuokra/Auto_vuokraus/Auto_vuokraus/KAYTTAJA.cs
+++ Marko ja mika/Autovuokra/Auto_vuokraus/Auto_vuokraus/KAYTTAJA.cs
@@ -1,5 +1,4 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,29 +8,5 @@ namespace Auto_vuokraus
 {
     internal class KAYTTAJA
     {
-        YHDISTA yhdista = new YHDISTA();
-        public bool lisaaKayttaja(string id, string user, string pass)
-        {
-            MySqlCommand cmd = new MySqlCommand();
-            string kysely = "INSERT INTO `kayttaja`(`id`, `user`, `pass`) VALUES('@id', '@user', '@pass')";
-            cmd.CommandText = kysely;
-            cmd.Connection = yhdista.otaYhteys();
-            cmd.Parameters.Add =("id", MySqlDbType.VarChar).Value = id;
-            cmd.Parameters.Add = ("user", MySqlDbType.VarChar).Value = user;
-            cmd.Parameters.Add = ("pass", MySqlDbType.VarChar).Value = pass;
-
-            yhdista.avaaXhteys();
-            if(cmd.ExecuteNonQuery() = 1)
-            {
-                yhdista.suljeYhteys();
-                return true;
-            }
-            else
-            {
-                yhdista.suljeYhteys();
-                return false;
-            }
-        }
-
     }
 }
